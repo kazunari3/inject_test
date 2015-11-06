68628d0979b919c3a8c0f59d07a1b49397c800d8
kazunari3 <canser@bu.iij4u.or.jp>
2015-11-06 21:41:26 +0900

    "Autowired" example
    @Autowired
    @Component

---
+++ src/main/java/com/neo/hello/Eye.java
@@ -0,0 +1,10 @@
+package com.neo.hello;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Eye {
+	void open() {
+		System.out.println("open you eyes!");
+	}
+}
--- src/main/java/com/neo/hello/Person.java
+++ src/main/java/com/neo/hello/Person.java
@@ -1,7 +1,14 @@
 package com.neo.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Person {
+	
+	@Autowired
+	Eye eye;
+	
     public void greet() {
         System.out.println("Hello, World!!");
+        eye.open();
     }
 }
